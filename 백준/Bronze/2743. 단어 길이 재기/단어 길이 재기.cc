b8de8c66840ede2ee7ae5fb9705e01781545de8d
heejin Jang <105128163+heeejini@users.noreply.github.com>
2024-05-11 22:18:13 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define pii pair<int, int>
+#define ll long long
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    
+    string s;
+
+    cin >> s;
+
+    cout << s.size();
+    
+    return 0;
+}
