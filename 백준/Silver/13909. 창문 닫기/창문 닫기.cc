653195599a16e97d830e3c5ac58cb825e76dc2f4
heejin Jang <105128163+heeejini@users.noreply.github.com>
2024-09-06 14:42:01 +0900

    [Silver V] Title: 창문 닫기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define pii pair<int, int>
+#define ll long long
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    
+    ll n;
+    cin >> n;
+
+    cout << (ll)sqrt(n) << endl; 
+    
+    return 0;
+}
